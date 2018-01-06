00747165d2b6eb621b8c1447799e49af55274c5a
Torren Martin <torren.martin89@gmail.com>
2018-01-06 04:00:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,19 @@ function destructivelyRemoveLastKitten(){
   return kittens.pop();
 }
 function appendKitten(name){
-  newArray = kittens.
+  var newArray = [...kittens, name];
+  return newArray;
+}
+function prependKitten(name){
+  var newArray = [name, ...kittens];
+  return newArray;
+}
+function removeLastKitten(){
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+
+function removeFirstKitten(){
+  var newArray = kittens.slice(1);
+  return newArray;
 }
